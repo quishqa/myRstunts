fc173cc2885bc774faf50425d09fd36363652b07
quishqa <mgavidiac@gmail.com>
2019-04-23 13:14:12 -0300

    Update bg_comparation.R
    function to read munich output, street link concentration mean for urban zone

--- bg_comparation.R
+++ bg_comparation.R
@@ -5,15 +5,22 @@ library(openair)
 munich <- '~/munich/SinG-training/MUNICH/munich-pre_postprocessing/preprocessing/'
 results.path <- 'bg_jund_test/results'
 
-all.path <- paste0(munich, results.path)
+LoadMunichOutput <- function(all.path){
+  files <- list.files(all.path, pattern = '201802', full.names = T )
+  files.dates <- list.files(all.path, pattern = '2018')
+  
+  output <- lapply(files, 
+                   function(x) read.table(x,header = F, sep = '\t', 
+                                          stringsAsFactors = F,
+                                          dec = '.', 
+                                          col.names = c('link', 'o3', 'no', 'no2', 'nothing')))
+  
+  h.means <- lapply(output, colMeans)
+  h.df <- as.data.frame(do.call(rbind, h.means))
+  h.df$date <- as.POSIXct(strptime(files.dates, "%Y%m%d_%H-%M"), tz = 'UTM')
+  h.df <- h.df[, c('date', 'o3', 'no', 'no2')]
+  return(h.df)
+}
 
-files <- list.files(all.path, pattern = '201802', full.names = T )
 
-output <- lapply(files, 
-                 function(x) read.table(x,header = F, sep = '\t', 
-                                        stringsAsFactors = F,
-                                        dec = '.', 
-                                        col.names = c('link', 'o3', 'no', 'no2', 'nothing')))
-
-h.means <- lapply(output, colMeans)
-h.df <- as.data.frame(do.call(rbind, h.means))
+pin.jund <- LoadMunichOutput(paste0(munich, results.path)
